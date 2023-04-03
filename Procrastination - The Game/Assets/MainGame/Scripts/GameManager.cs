ec22450ebcfe06cea937e8865cae514e61628657
Owen Liang <owenliang3139@gmail.com>
2023-04-03 10:40:54 -0400

    Added Typing Game Into Game

--- Procrastination - The Game/Assets/MainGame/Scripts/GameManager.cs
+++ Procrastination - The Game/Assets/MainGame/Scripts/GameManager.cs
@@ -1,19 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    public GameObject eventSystemPrefab;
+
+    void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        if (!GameObject.Find("EventSystem"))
+        {
+            if (eventSystemPrefab != null)
+            {
+                Instantiate(eventSystemPrefab);
+            }
+            else
+            {
+                Debug.LogError("EventSystem prefab is null.");
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!GameObject.Find("EventSystem"))
+        {
+            if (eventSystemPrefab != null)
+            {
+                Instantiate(eventSystemPrefab);
+            }
+            else
+            {
+                Debug.LogError("EventSystem prefab is null.");
+            }
+        }
     }
 }
