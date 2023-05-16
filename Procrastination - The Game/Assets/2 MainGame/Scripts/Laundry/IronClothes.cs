92909142ffbcd7277ffa6fbfbaa24205f4a71a8f
Owen Liang <owenliang3139@gmail.com>
2023-05-15 22:40:07 -0400

    Edited Laundry Task

--- Procrastination - The Game/Assets/2 MainGame/Scripts/Laundry/IronClothes.cs
+++ Procrastination - The Game/Assets/2 MainGame/Scripts/Laundry/IronClothes.cs
@@ -10,12 +10,16 @@ public class IronClothes : MonoBehaviour
     public TextMeshProUGUI instructionText; // Assign the TextMeshProUGUI component in the Inspector
     public WashingMachine washingMachine; // Assign the WashingMachine component in the Inspector
     public float taskDoneDisplayTime = 3f; // The time (in seconds) to display the "Task Done" message
+    public Image ironTableImage; // Assign the Image component of the iron table in the Inspector
+    public Sprite[] ironTableImages; // Assign the array of images in the Inspector
 
     private bool isTaskComplete = false; // Initially, the task is not complete
+    private bool hasImageChanged = false; // Initially, the image has not been changed
 
     void Start()
     {
         instructionText.text = ""; // Hide the instruction text at the start
+        ironTableImage.sprite = ironTableImages[0]; // Display the initial image
     }
 
     void Update()
@@ -23,6 +27,13 @@ public class IronClothes : MonoBehaviour
         if (washingMachine.isLaundryDone && !isTaskComplete)
         {
             instructionText.text = "Hold Space to Iron";
+
+            // Change the iron table image once
+            if (!hasImageChanged)
+            {
+                ironTableImage.sprite = ironTableImages[1]; // Display the second image
+                hasImageChanged = true; // The image has been changed
+            }
 
             if (Input.GetKey(KeyCode.Space))
             {
